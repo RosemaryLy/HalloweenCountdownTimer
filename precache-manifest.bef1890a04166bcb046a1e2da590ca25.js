e92e4248874153ee069d34af33000a2ac5acbef0
RosemaryLy <rosey.ly@hotmail.com>
2020-09-30 20:08:25 -0400

    Updates

---
+++ precache-manifest.bef1890a04166bcb046a1e2da590ca25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395756793af616629cb1eb79256fe54b",
+    "url": "/HalloweenCountdownTimer/index.html"
+  },
+  {
+    "revision": "f06e86d952f4ab596e88",
+    "url": "/HalloweenCountdownTimer/static/css/main.65a8f13f.chunk.css"
+  },
+  {
+    "revision": "49b9e6fce51847dc6a55",
+    "url": "/HalloweenCountdownTimer/static/js/2.bea52edd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HalloweenCountdownTimer/static/js/2.bea52edd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f06e86d952f4ab596e88",
+    "url": "/HalloweenCountdownTimer/static/js/main.dd01a8ca.chunk.js"
+  },
+  {
+    "revision": "4d96932804081e7cb0e2",
+    "url": "/HalloweenCountdownTimer/static/js/runtime-main.58665a8f.js"
+  },
+  {
+    "revision": "4dd0f7ce4b35f902fc2557125a1a0b14",
+    "url": "/HalloweenCountdownTimer/static/media/beth-teutschmann-ed5KySP1tfQ-unsplash.4dd0f7ce.jpg"
+  }
+]);
